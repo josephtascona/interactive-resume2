adc42fb7c632274c704c8330238617f533bce1fd
Joseph Tascona <joseph.tascona@gmail.com>
2024-04-28 14:20:46 -0400

    Added more logic

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ window.addEventListener('beforeunload', function () {
 
 document.addEventListener("DOMContentLoaded", function () {
     var box = document.querySelector('#box');
+    var spacebar = document.querySelector('#spacebar');
     var forwardInterval;
     var backwardInterval;
     var intervalOn = "";
@@ -16,124 +17,37 @@ document.addEventListener("DOMContentLoaded", function () {
 
     let num = -1;
     let i = 0;
-    var text = ["Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus magni aut, ea soluta corrupti, ex sint nemo veritatis repellendus veniam facilis ipsum fuga, neque eaque autem minima ratione! Aliquid, id?", ""];
+    var text = ["Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus magni aut, ea soluta corrupti, ex sint nemo veritatis repellendus veniam facilis ipsum fuga, neque eaque autem minima ratione! Aliquid, id?", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus magni aut, ea soluta corrupti, ex sint nemo veritatis repellendus veniam facilis ipsum fuga, neque eaque autem minima ratione! Aliquid, id?"];
     var typingDiv;
+    var paused = false;
 
-    // Listen for keydown event
-    document.addEventListener("keydown", function (event) {
-        if (event.key === "d") {
-            if (intervalOn === "forwardInterval") {
-                clearInterval(forwardInterval);
-            } else if (intervalOn === "backwardInterval") {
-                clearInterval(backwardInterval);
-            }
-            intervalOn = "forwardInterval";
-            if (boxLeft < 2105 && boxTop === 530) {
-                // Move the box 5 pixels to the right
-                boxLeft += 5;
-                box.style.left = boxLeft + "px";
-            } else if (boxLeft === 2105 && boxTop < 2150) {
-                boxTop += 5;
-                box.style.top = boxTop + "px";
-            }
-            if (boxLeft === 2105 && boxTop === 800) {
-                window.scrollTo(1550, 700);
-            }
-            if (boxLeft == 1750) {
-                window.scrollTo(1550, 0);
-            }
-            if (boxLeft == 375) {
-                num += 1
-                typingDiv = document.getElementById('typing-effect' + num);
-            }
-            if (num != -1) {
-                if (typingDiv.innerHTML != text[num]) {
+    function startTyping() {
+        var typingInterval = setInterval(function () {
+            if (num !== -1) {
+                if (typingDiv.innerHTML !== text[num]) {
                     if (i < text[num].length) {
                         typingDiv.innerHTML += text[num].charAt(i);
                         i++;
                     }
                 } else {
-                    i = 0
+                    i = 0;
+                    paused = false;
+                    intervalOn = "";
+                    spacebar.style.display = "block";
+                    clearInterval(typingInterval);
                 }
             }
-        } 
-        
-        if (event.key === "s") {
-            // Stop the repeating action when the key is released
-            if (intervalOn === "forwardInterval") {
-                clearInterval(forwardInterval);
-                intervalOn = "";
-            } else if (intervalOn === "backwardInterval") {
-                clearInterval(backwardInterval);
-                intervalOn = "";
-            } else {
-                intervalOn = "forwardInterval";
-                forwardInterval = setInterval(function () {
-                    if (boxLeft < 2105 && boxTop === 530) {
-                        // Move the box 5 pixels to the right
-                        boxLeft += 5;
-                        box.style.left = boxLeft + "px";
-                    } else if (boxLeft === 2105 && boxTop < 2150) {
-                        boxTop += 5;
-                        box.style.top = boxTop + "px";
-                    }
-                    if (boxLeft === 2105 && boxTop === 800) {
-                        window.scrollTo(1550, 700);
-                    }
-                    if (boxLeft == 1750) {
-                        window.scrollTo(1550, 0);
-                    }
-                    if (boxLeft == 375) {
-                        num += 1
-                        typingDiv = document.getElementById('typing-effect' + num);
-                    }
-                    if (num != -1) {
-                        if (typingDiv.innerHTML != text[num]) {
-                            if (i < text[num].length) {
-                                typingDiv.innerHTML += text[num].charAt(i);
-                                i++;
-                            }
-                        } else {
-                            i = 0
-                        }
-                    }
-                }, 35);
-            }
-        }
-        
-        if (event.key == "a") {
-            if (intervalOn === "forwardInterval") {
-                clearInterval(forwardInterval);
-            } else if (intervalOn === "backwardInterval") {
-                clearInterval(backwardInterval);
-            }
-            intervalOn = "backwardInterval";
-            if (boxLeft >= 305) {
-                if (boxLeft <= 2105 && boxTop === 530) {
-                    // Move the box 5 pixels to the right
-                    boxLeft -= 5;
-                    box.style.left = boxLeft + "px";
-                } else if (boxLeft === 2105 && boxTop < 2150) {
-                    boxTop -= 5;
-                    box.style.top = boxTop + "px";
-                }
-            }
-            if (boxLeft === 2105 && boxTop === 800) {
-                window.scrollTo(1550, 0);
-            }
-            if (boxLeft == 1750) {
-                window.scrollTo(0, 0);
-            }
-            if (boxLeft == 375) {
-                num -= 1
-            }
-        }
-    });
+        }, 35); // Adjust interval as needed
+    }
 
-    document.addEventListener("keyup", function (event) {
-        if (event.key === "d") {
-            // Start a timer to repeat the action every 35 milliseconds
-            forwardInterval = setInterval(function () {
+    // Listen for keydown event
+    document.addEventListener("keydown", function (event) {
+        if (!paused) {
+            if (event.key === "d") {
+                spacebar.style.display = "none";
+                clearInterval(forwardInterval);
+                clearInterval(backwardInterval);
+                intervalOn = "forwardInterval";
                 if (boxLeft < 2105 && boxTop === 530) {
                     // Move the box 5 pixels to the right
                     boxLeft += 5;
@@ -142,32 +56,73 @@ document.addEventListener("DOMContentLoaded", function () {
                     boxTop += 5;
                     box.style.top = boxTop + "px";
                 }
-                if (boxLeft == 1750) {
-                    window.scrollTo(1550, 0);
-                }
                 if (boxLeft === 2105 && boxTop === 800) {
                     window.scrollTo(1550, 700);
                 }
-                if (boxLeft == 375) {
-                    num += 1
+                if (boxLeft == 1750) {
+                    window.scrollTo(1550, 0);
+                }
+                if (boxLeft == 700 && num === -1) {
+                    num += 1;
                     typingDiv = document.getElementById('typing-effect' + num);
+                    paused = true;
+                    startTyping();
                 }
-                if (num != -1) {
-                    if (typingDiv.innerHTML != text[num]) {
-                        if (i < text[num].length) {
-                            typingDiv.innerHTML += text[num].charAt(i);
-                            i++;
+                if (boxLeft == 1200 && num === 0) {
+                    num += 1;
+                    typingDiv = document.getElementById('typing-effect' + num);
+                    paused = true;
+                    startTyping();
+                }
+            }
+
+            if (event.key === "s") {
+                spacebar.style.display = "none";
+                // Stop the repeating action when the key is released
+                if (intervalOn == "forwardInterval" || intervalOn == "backwardInterval") {
+                    clearInterval(forwardInterval);
+                    clearInterval(backwardInterval);
+                    intervalOn = "";
+                } else {
+                    intervalOn = "forwardInterval";
+                    forwardInterval = setInterval(function () {
+                        if (boxLeft < 2105 && boxTop === 530) {
+                            // Move the box 5 pixels to the right
+                            boxLeft += 5;
+                            box.style.left = boxLeft + "px";
+                        } else if (boxLeft === 2105 && boxTop < 2150) {
+                            boxTop += 5;
+                            box.style.top = boxTop + "px";
                         }
-                    } else {
-                        i = 0
-                    }
+                        if (boxLeft === 2105 && boxTop === 800) {
+                            window.scrollTo(1550, 700);
+                        }
+                        if (boxLeft == 1750) {
+                            window.scrollTo(1550, 0);
+                        }
+                        if (boxLeft == 700 && num === -1) {
+                            num += 1;
+                            typingDiv = document.getElementById('typing-effect' + num);
+                            paused = true;
+                            startTyping();
+                            clearInterval(forwardInterval);
+                        }
+                        if (boxLeft == 1200 && num === 0) {
+                            num += 1;
+                            typingDiv = document.getElementById('typing-effect' + num);
+                            paused = true;
+                            startTyping();
+                            clearInterval(forwardInterval);
+                        }
+                    }, 35);
                 }
-            }, 35);
-        } 
-        
-        if (event.key === "a") {
-            // Start a timer to repeat the action every 35 milliseconds
-            backwardInterval = setInterval(function () {
+            }
+
+            if (event.key == "a") {
+                spacebar.style.display = "none";
+                clearInterval(forwardInterval);
+                clearInterval(backwardInterval);
+                intervalOn = "backwardInterval";
                 if (boxLeft >= 305) {
                     if (boxLeft <= 2105 && boxTop === 530) {
                         // Move the box 5 pixels to the right
@@ -184,10 +139,69 @@ document.addEventListener("DOMContentLoaded", function () {
                 if (boxLeft == 1750) {
                     window.scrollTo(0, 0);
                 }
-                if (boxLeft == 375) {
-                    num -= 1
-                }
-            }, 35);
+            }
+        }
+    });
+
+    document.addEventListener("keyup", function (event) {
+        if (!paused) {
+            if (event.key === "d") {
+                intervalOn = "forwardInterval";
+                // Start a timer to repeat the action every 35 milliseconds
+                forwardInterval = setInterval(function () {
+                    if (boxLeft < 2105 && boxTop === 530) {
+                        // Move the box 5 pixels to the right
+                        boxLeft += 5;
+                        box.style.left = boxLeft + "px";
+                    } else if (boxLeft === 2105 && boxTop < 2150) {
+                        boxTop += 5;
+                        box.style.top = boxTop + "px";
+                    }
+                    if (boxLeft == 1750) {
+                        window.scrollTo(1550, 0);
+                    }
+                    if (boxLeft === 2105 && boxTop === 800) {
+                        window.scrollTo(1550, 700);
+                    }
+                    if (boxLeft == 700 && num === -1) {
+                        num += 1;
+                        typingDiv = document.getElementById('typing-effect' + num);
+                        paused = true;
+                        startTyping();
+                        clearInterval(forwardInterval);
+                    }
+                    if (boxLeft == 1200 && num === 0) {
+                        num += 1;
+                        typingDiv = document.getElementById('typing-effect' + num);
+                        paused = true;
+                        startTyping();
+                        clearInterval(forwardInterval);
+                    }
+                }, 35);
+            }
+
+            if (event.key === "a") {
+                intervalOn = "backwardInterval"
+                // Start a timer to repeat the action every 35 milliseconds
+                backwardInterval = setInterval(function () {
+                    if (boxLeft >= 305) {
+                        if (boxLeft <= 2105 && boxTop === 530) {
+                            // Move the box 5 pixels to the right
+                            boxLeft -= 5;
+                            box.style.left = boxLeft + "px";
+                        } else if (boxLeft === 2105 && boxTop < 2150) {
+                            boxTop -= 5;
+                            box.style.top = boxTop + "px";
+                        }
+                    }
+                    if (boxLeft === 2105 && boxTop === 800) {
+                        window.scrollTo(1550, 0);
+                    }
+                    if (boxLeft == 1750) {
+                        window.scrollTo(0, 0);
+                    }
+                }, 35);
+            }
         }
     });
 });
